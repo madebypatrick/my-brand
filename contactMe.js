3cfbe3340cea54bcbbea056ccdd15aef74583ee4
madebypatrick <mugwanezapatrick6@gmail.com>
2023-02-17 16:46:49 +0200

    I connected contact form to local storage

--- contactMe.js
+++ contactMe.js
@@ -3,13 +3,16 @@ let form=document.getElementById('contactForm');
 
 // form refresh state
 form.addEventListener('submit', (e)=>{
-    let hisname=document.getElementById('name');
+    let theName=document.getElementById('name');
     let email=document.getElementById('email');
+    let fromWhere=document.getElementById('about')
+
     let message=document.getElementById('comments');
     // set value
 
-    hisname.value='';
+    theName.value='';
     email.value='';
+    fromWhere.value='';
     message.value='';
 
 
@@ -18,18 +21,27 @@ form.addEventListener('submit', (e)=>{
 
 // start to send
 sendBtn.addEventListener('click',(e)=>{
-    let hisname=document.getElementById('name');
+    let theName=document.getElementById('name');
     let email=document.getElementById('email');
+    let fromWhere=document.getElementById('about');
     let message=document.getElementById('comments');
+    if(theName.value==='' || email.value===''|| fromWhere.value===''|| message.value=-=''){
+        alert("fill every field");
+        
+    }
 
     // set value and local storage
-    hisname=hisname.value;
-    localStorage.setItem('hisname',hisname);
+    theName=theName.value;
+    localStorage.setItem('theName',theName);
     email=email.value;
     localStorage.setItem('email',email);
+
+    fromWhere=fromWhere.value;
+    localStorage.setItem('fromWhere',fromWhere);
+
 
     message=message.value;
     localStorage.setItem('message',message);
 
-
+   
 });
