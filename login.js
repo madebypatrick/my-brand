41a2e2836ce578d5e020ae328b3f2d8f0de73edf
madebypatrick <mugwanezapatrick6@gmail.com>
2023-02-17 12:47:07 +0200

    I added login, profile JS files

---
+++ login.js
@@ -0,0 +1,40 @@
+//  validate login form
+
+// document.querySelector(".sending").onclick = function (e) {
+//   e.preventDefault();
+//   var email3 = document.querySelector("#logEmail").value,
+//     password3 = document.querySelector("#password").value;
+
+//   if (password3 == "" || email3 =="") {
+//     swal("NOT YET!", "Please ferify your credentials!", "error");
+//     return false;
+//   }
+//   return true;
+// };
+
+// ==========================
+// LOGIN  localstorage
+const email = document.getElementById('logEmail');
+const password = document.getElementById('password');
+const login = document.getElementById('sending');
+
+users = JSON.parse(localStorage.getItem('users')) || [];
+
+function signIn() {
+  // check if user exists
+  const targetUser = users.find(user => user.email == email.value);
+  if(targetUser && targetUser.password == password.value) {
+    localStorage.setItem('currentUser', JSON.stringify(targetUser))
+    window.location.href = "adminHome.html";
+  } else if(targetUser && targetUser.password != password.value) {
+    swal("NOT YET!", "Please ferify your credentials!", "error");
+    
+  } else {
+    // user doesn't exist
+    alert('not registered, go to signup?');
+    window.location.href = 'signup.html';
+  }
+}
+
+login.onclick = signIn;
+
--- signup.js
+++ signup.js
@@ -1,137 +1,110 @@
 
 // ===============Validate signup form=======================
-const form=document.getElementById('form');
-const fname=document.getElementById('fname');
-const email=document.getElementById('logEmailUp');
-const password=document.getElementById('passwordUp');
-const password2=document.getElementById('passwordConf');
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-
-  validateInputs();
-});
 
 
-const setError=(element, message)=>{
-  const inputControl = element.parentElement;
-  const errorDisplay =inputControl.querySelector('.error');
-  errorDisplay.innerText = message;
-  inputControl.classList.add('error');
-  inputControl.classList.remove('success');
 
-}
-const setSuccess=element =>{
-  const inputControl=element.parentElement;
-  const errorDisplay=inputControl.querySelector('.error');
-errorDisplay.innerText='';
-inputControl.classList.add('success');
-inputControl.classList.remove('error');
-};
 
-const isValidEmail=email =>{
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
+// const form=document.getElementById('form');
+// const fname=document.getElementById('fname');
+// const email=document.getElementById('logEmailUp');
+// const password1=document.getElementById('passwordUp');
+// const password2=document.getElementById('passwordConf');
 
-const validateInputs=()=>{
-  const fnameValue=fname.value.trim();
-  const emailValue=email.value.trim();
-  const passwordValue=password.value.trim();
-  const password2Value=password2.value.trim();
-  if(fnameValue===''){
-    setError(fname,'Full name is required');
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
 
-  }else{
-    setSuccess(fname);
+//   validateInputs();
+// });
 
-  }
-  if(emailValue===''){
-    setError(email,'Email is required');
-  }else if (!isValidEmail(emailValue)){
-    setError(email, 'Provide a valid email address');
 
-  }else{
-    setSuccess(email);
-  }
-  if(passwordValue===''){
-    setError(password,"Password is required");
-  }else if(passwordValue.length<6){
-    setError(password,'Password must be at least 6 characters');
-  }else{
-    setSuccess(password);
-  }
-  if(password2Value===''){
-    setError(password2,'Please confirm your password');
-  }else if(password2Value !== passwordValue){
-    setError(password2,"Passwords don't match");
-  }else{
-    setSuccess(password2);
-  }
+// const setError=(element, message)=>{
+//   const inputControl = element.parentElement;
+//   const errorDisplay =inputControl.querySelector('.error');
+//   errorDisplay.innerText = message;
+//   inputControl.classList.add('error');
+//   inputControl.classList.remove('success');
 
-};
+// }
+// const setSuccess=element =>{
+//   const inputControl=element.parentElement;
+//   const errorDisplay=inputControl.querySelector('.error');
+// errorDisplay.innerText='';
+// inputControl.classList.add('success');
+// inputControl.classList.remove('error');
+// };
+
+// const isValidEmail=email =>{
+//   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(String(email).toLowerCase());
+// }
+
+// const validateInputs=()=>{
+//   const fnameValue=fname.value.trim();
+//   const emailValue=email.value.trim();
+//   const password1Value=password1.value.trim();
+//   const password2Value=password2.value.trim();
+//   if(fnameValue===''){
+//     setError(fname,'Full name is required');
+
+//   }else{
+//     setSuccess(fname);
+
+//   }
+//   if(emailValue===''){
+//     setError(email,'Email is required');
+//   }else if (!isValidEmail(emailValue)){
+//     setError(email, 'Provide a valid email address');
+
+//   }else{
+//     setSuccess(email);
+//   }
+//   if(password1Value===''){
+//     setError(password1,"Password is required");
+//   }else if(password1Value.length<6){
+//     setError(password1,'Password must be at least 6 characters');
+//   }else{
+//     setSuccess(password1);
+//   }
+//   if(password2Value===''){
+//     setError(password2,'Please confirm your password');
+//   }else if(password2Value !== password1Value){
+//     setError(password2,"Passwords don't match");
+//   }else{
+//     setSuccess(password2);
+//   }
+
+// };
 
 
 
 // =================Local Storage============================
 
+// identify forminput field values by time of clicking submit
+const nameField = document.getElementById('fname');
+const emailField = document.getElementById('logEmailUp');
+const password = document.getElementById('passwordUp');
+const role = document.getElementById('role');
+// signup button
+const signup = document.getElementById('sendingUp');
 
 
+users = JSON.parse(localStorage.getItem('users')) || [];
 
-// const nameField = document.getElementById('fname');
-// const emailField = document.getElementById('logEmailUp');
-// const passwordz = document.getElementById('passwordConf');
+function saveUser() {
+  let user = {};
+  user.firstName = nameField.value.split(' ')[0];
+  user.lastName = nameField.value.split(' ')[1];
+  user.email = emailField.value
+  user.password = password.value
+  users.push(user);
+  const stringUsers = JSON.stringify(users);
+  localStorage.setItem('users', stringUsers);
+}
 
-// // signUp button
-// const signup= document.getElementById('sendingUp');
-// const listUsers=document.getElementById('listUsers');
+function displayUsers() {
+  users.forEach((user, index) => {
+    listUsers.innerHTML += `<li>user number ${index} is ${user.firstName} ${user.lastName}</li>`
+  })
+}
 
-// // display users button
-
-// const showUsers = document.getElementById('showUsers');
-//  users = JSON.parse(localStorage.getItem('users')) || [];
-
-//  function saveUser(){
-//   let user={};
-//   user.firstName=nameField.value.split(' ')[0];
-//   user.lastNime=nameField.value.split(' ')[1];
-//   user.email=emailField.value
-//   user.passwordz=passwordz.value
-//   user.role=role.value
-//   users.push(user);
-//   const stringUsers =JSON.stringify(users);
-//   localStorage.setItem('users', stringUsers);
-//  }
-
-//  functiondisplayUsers(){
-//   users.forEach((user,index) =>{
-//     listUsers.innerHTML += `<li>user number ${index} is ${user.firstName} ${user.LastName}</li>`
-//   })
-//  }
-
-
-
-
-
-
-
-
-
-
-
-//  validate login form 
-
-
-
-// document.querySelector(".sending").onclick = function (e) {
-//   e.preventDefault();
-//   var email3 = document.querySelector("#logEmail").value,
-//     password3 = document.querySelector("#password").value;
-  
-
-//   if (password3 == "" || email3 =="") {
-//     swal("NOT YET!", "Please ferify your credentials!", "error");
-//     return false;
-//   }    
-//   return true;
-// };
+signup.onclick = saveUser;
