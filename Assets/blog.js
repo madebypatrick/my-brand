fa4a82c297c3d6cfacf6a4a67f02707ffa7f3264
madebypatrick <mugwanezapatrick6@gmail.com>
2023-03-13 17:07:38 +0200

    fixed blog page bug

--- Assets/blog.js
+++ Assets/blog.js
@@ -1,18 +1,18 @@
-const blogSection = document.querySelector(".gridItem1");
-const blogsDetails = JSON.parse(localStorage.getItem("postList")) || [];
-// console.log(blogsDetails);
+// const blogSection = document.querySelector(".gridItem1");
+// const blogsDetails = JSON.parse(localStorage.getItem("postList")) || [];
+// // console.log(blogsDetails);
 
-blogsDetails.forEach((item, index) => {
-    blogSection.insertAdjacentHTML('afterbegin',`
-    <img class="gridBoxImage" src="${item.image}">              
-            <h6>${item.category}</h6>
-        <a href="./Blog/machineLearning1.html">
-          <p>${item.title}</p></a>
+// blogsDetails.forEach((item, index) => {
+//     blogSection.insertAdjacentHTML('afterbegin',`
+//     <img class="gridBoxImage" src="${item.image}">              
+//             <h6>${item.category}</h6>
+//         <a href="./Blog/machineLearning1.html">
+//           <p>${item.title}</p></a>
 
-        <p>${item.date}</p>       
+//         <p>${item.date}</p>       
                     
-        `)
-});
+//         `)
+// });
 
 
 
